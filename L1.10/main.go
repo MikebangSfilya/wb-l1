c05b224729d56e2d650c84a818fc26c62c27dfc5
Sfilya <Typegoot@gmail.com>
2026-02-15 01:38:29 +0300

    L1.10

---
+++ L1.10/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	temps := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	m := make(map[int][]float64)
+
+	for _, v := range temps {
+		k := int(v/10) * 10
+		m[k] = append(m[k], v)
+	}
+	fmt.Println(m)
+
+}
